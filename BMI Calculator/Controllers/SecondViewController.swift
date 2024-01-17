36b425948a3a6a1d4965951abb4d0372a0826be4
smease12 <scmease93@gmail.com>
2024-01-16 20:54:41 -0500

    Add SecondViewController

---
+++ BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Serena  on 1/16/24.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController{
+    
+}
